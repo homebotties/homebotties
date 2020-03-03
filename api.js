3ecee3c71461b413914bb90604b06accc10de131
Scott Raymond <sco@scottraymond.net>
2020-03-03 01:54:48 +0000

    simplifying default bot

--- api.js
+++ api.js
@@ -1,13 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server-express');
-import { lights, on, off, set, state } from './hue';
-import { init, setDriveMode } from './car';
 import { status, logs, createLog } from './bot';
 
 export const typeDefs = gql`
-  type Light {
-    id: String!
-    name: String!
-  }
   type Log {
     text: String
   }
@@ -15,50 +9,21 @@ export const typeDefs = gql`
     status: String!
     logs: [Log!]!
   }
-  enum DriveMode {
-    STOP
-    GO
-    BACK
-    LEFT
-    RIGHT
-  }
   type Query {
-    greeting(name: String): String!
-    lights: [Light!]!
-    camera: String,
     bot: Bot!
   }
   type Mutation {
-    setLight(name: String!, on: Boolean, bri: Int): Boolean!
-    setDriveMode(driveMode: DriveMode!, v: Float): DriveMode
     createLog(text: String): Log
   }
 `;
 
 export const resolvers = {
   Query: {
-    greeting(parent, args, context) {
-      return `Hello, ${args.name || 'world'}!`;
-    },
-    lights(parent, args, context) {
-      return lights;
-    },
-    camera(parent, args, context) {
-      return 'http://scobot:8080/stream/video.mjpeg'
-    },
     bot(parent, args, context) {
-      return bot;
+      return { status };
     },
   },
   Mutation: {
-    async setLight(parent, { name, on, bri }, context) {
-      set(name, state().bri(bri));// TODO on-ness
-      return true;
-    },
-    async setDriveMode(parent, { driveMode, v }, context) {
-      await setDriveMode(driveMode, v);
-      return { value: driveMode };
-    },
     async createLog(parent, args, context) {
       return bot.createLog(args)
     }
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 import { apiServer } from './api';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 apiServer.applyMiddleware({ app, path: '/api' });
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/app.html'));
 });
 
 app.listen(port, () => console.log(`Bot app running on port ${port}`))
