f5a83c21eda4abda765d9cbef2918d5224bc6592
Scott Raymond <sco@scottraymond.net>
2020-03-07 11:32:42 -0800

    simplify server

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-import { apiServer } from './api';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-apiServer.applyMiddleware({ app, path: '/api' });
+import { apiServer as api } from './api';
 
 function sendFile(filename) {
   return (req, res) => res.sendFile(path.join(__dirname + `/${filename}`));
 }
 
-app.get('/', sendFile('app.html')); // TODO: get app module from API?
+const router = express();
 
+api.applyMiddleware({ router, path: '/api' }); // TODO: how to express this as not middleware?
+router.get('/', sendFile('app.html')); // TODO: get app module from API?
+roter.get('/README', sendFile('README.md')); // TODO md render in app shell?
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Bot app running on port ${port}`))
 
